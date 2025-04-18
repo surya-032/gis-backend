aa56cbc5d7a6e8d788e48f4c420f3889c23a3369
surya-032 <114859751+surya-032@users.noreply.github.com>
2025-04-18 19:46:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 require("dotenv").config();
-
 const authRoutes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 11032;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes API (auth, register, login, dll)
-app.use("/api", authRoutes);
+app.use("/", authRoutes);
 
-// Serve static files (hasil build React)
-app.use(express.static(path.join(__dirname, "../public")));
-
-// React Router fallback (biar bisa refresh halaman non-root)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
-
-// Start server
 app.listen(PORT, () => {
-  console.log(`✅ Server berjalan di http://localhost:${PORT}`);
+    console.log(`Server berjalan di http://localhost:${PORT}`);
 });
